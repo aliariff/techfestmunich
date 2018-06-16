9486066d60444d126e2cc8652988595f02b32d00
Vincent <vincent.karas@tum.de>
2018-06-16 13:57:41 +0200

    CarInfo

--- socket_io/client.js
+++ socket_io/client.js
@@ -1,6 +1,22 @@
 var io = require('socket.io-client');
 var socket = io('http://localhost:3000?clientId=car_2');
 
+
+int id = 2;
+double speed = 0;
+double position = 0;
+
 socket.on('slow_down', function(data) {
     console.log(data);
+});
+
+socket.on('req_info', function(data){
+	
+	if(data['id'] == this.id){
+		io.sockets.emit('send_info', {
+			id: this.id,
+			speed: this.speed,
+			position: this.position
+		});
+	}
 });
