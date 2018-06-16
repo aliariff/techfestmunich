bb9a8d2359621fa5070b1b9df8464a384b116bce
Vincent <vincent.karas@tum.de>
2018-06-16 12:54:26 +0200

    pseudocode

--- socket_io/server.js
+++ socket_io/server.js
@@ -16,21 +16,63 @@ io.on('connection', function(socket) {
 
 
     socket.on('car_event', function(data) {
-<<<<<<< HEAD
+
         // receive car event data
         // if there is obstruction, emit to slow_down / full brake
         //  socket.emit('slow_down', {
         //     speed: -10
         // });
 		
-=======
         console.log(data);
         if (data['status'] == 'obstacle') {
+		  
+		  //slow down Truck 1
+		  
+		  double pos1 = data['position'];
+		  double v1 = data['speed']M;
+		  double obstaclepos = data['obstacleposition'];
+		
+		  double distToObst = obstaclepos - pos1;
+			
+		  double safetydistance1 = 0; // to obstacle
+		  
+		  double sddistance1 = distToObst - safetydistance;
+		  
+		  dt1 = sddistance11/v1;
+		  
+		  int targetID = 1;
+			
           io.sockets.emit('slow_down', {
-             speed: -10
+			 id = targetID, 
+             speed: -v1,
+			 time: dt1
          });
+		 
+		 //slow down Truck 2
+		 
+			data2 = get(Truck2.info)
+			double pos2 = data2['position'];
+			double v2 = data2['speed'];
+			
+			double safetydistance2 = ?;
+		
+			double sddistance2 = obstaclepos - pos2 - ssdistance2;
+		 
+			dt2 = sddistance2/v2;
+			
+			int targetID2 = 2;
+			
+			io.sockets.emit('slow_down', {
+				id = targetID2,
+				speed: -v2,
+				time: dt2
+				
+			});
+		 
+		//same for Truck 3 
+		 
+		 
         }
 
->>>>>>> 197b7574fc05b0ab0c25cde83a49ba374367519e
     });
 });
