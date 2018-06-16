e5d181f9dd6e592099afddad00f1bb01d0f7d9a3
Aleksei Kolola <repoacc@yandex.ru>
2018-06-16 11:20:09 +0200

    Connected server and client

--- socket_io/server.js
+++ socket_io/server.js
@@ -13,11 +13,15 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log("client connected");
+
+
     socket.on('car_event', function(data) {
-        // receive car event data
-        // if there is obstruction, emit to slow_down / full brake
-        //  socket.emit('slow_down', {
-        //     speed: -10
-        // });
+        console.log(data);
+        if (data['status'] == 'obstacle') {
+          io.sockets.emit('slow_down', {
+             speed: -10
+         });
+        }
+
     });
 });
