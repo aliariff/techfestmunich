652a327360bfbedf1cdafae6fadfbec27080e9a0
Ali Ariff <ali.ariff12@gmail.com>
2018-06-16 13:09:40 +0200

    update

--- socket_io/server.js
+++ socket_io/server.js
@@ -18,67 +18,58 @@ io.on('connection', function(socket) {
     console.log(`client ${clientId} connected`);
 
     socket.on('car_event', function(data) {
-
-        // receive car event data
-        // if there is obstruction, emit to slow_down / full brake
-        //  socket.emit('slow_down', {
-        //     speed: -10
-        // });
-
         console.log(data);
         if (data['status'] == 'obstacle') {
-
-
-            //slow down Truck 1
-
-            var pos1 = data['position'];
-            var v1 = data['speed'];
-            var obstaclepos = data['obstacleposition'];
-
-            var distToObst = obstaclepos - pos1;
-
-            var safetydistance1 = 0; // to obstacle
-
-            var sddistance1 = distToObst - safetydistance;
-
-            dt1 = sddistance11 / v1;
-
-            var targetID = 1;
-
-            io.sockets.emit('slow_down', {
-                id: targetID,
-                speed: -v1,
-                time: dt1
-            });
-
-            //slow down Truck 2
-
-            data2 = get(Truck2.info)
-            var pos2 = data2['position'];
-            var v2 = data2['speed'];
-
-            var safetydistance2 = 10;
-
-            var sddistance2 = obstaclepos - pos2 - ssdistance2;
-
-            dt2 = sddistance2 / v2;
-
-            var targetID2 = 2;
-
-            io.sockets.emit('slow_down', {
-                id: targetID2,
-                speed: -v2,
-                time: dt2
-
-            });
-
-            //same for Truck 3
-
-
-
             io.sockets.emit('slow_down', {
                 speed: -10
             });
         }
     });
 });
+
+function test() {
+    //slow down Truck 1
+
+    var pos1 = data['position'];
+    var v1 = data['speed'];
+    var obstaclepos = data['obstacleposition'];
+
+    var distToObst = obstaclepos - pos1;
+
+    var safetydistance1 = 0; // to obstacle
+
+    var sddistance1 = distToObst - safetydistance;
+
+    dt1 = sddistance11 / v1;
+
+    var targetID = 1;
+
+    io.sockets.emit('slow_down', {
+        id: targetID,
+        speed: -v1,
+        time: dt1
+    });
+
+    //slow down Truck 2
+
+    data2 = get(Truck2.info)
+    var pos2 = data2['position'];
+    var v2 = data2['speed'];
+
+    var safetydistance2 = 10;
+
+    var sddistance2 = obstaclepos - pos2 - ssdistance2;
+
+    dt2 = sddistance2 / v2;
+
+    var targetID2 = 2;
+
+    io.sockets.emit('slow_down', {
+        id: targetID2,
+        speed: -v2,
+        time: dt2
+
+    });
+
+    //same for Truck 3
+}
