db26992b9abcfed10837e0fcaa01d99d3234148f
Aleksei Kolola <repoacc@yandex.ru>
2018-06-17 09:05:07 +0200

    Correct mathematical algorithm

--- MathProcessing/Computation.js
+++ MathProcessing/Computation.js
@@ -1,53 +1,51 @@
 var exports = module.exports = {};
 
-//e.g. params 11m/s , 10m/s , 50m , 30m , 1
+
+//--- All params in meters and seconds
+
+/*
+
+@params
+1st car"s speed;
+2nd or 3rd cars" speed;
+distance beetwen given cars;
+distance from first car to an obstacle;
+*/
 exports.calculate = function(speed_first, speed_second, distance, obstacle_distance) {
 
       console.log("=== Inside calculate function ===");
       console.log(speed_first);
       console.log(speed_second);
+      console.log(distance);
       console.log(obstacle_distance);
 
 
 var v;
 
-   //var deltav = -(speed_first - speed_second);
+   //--- var deltav = -(speed_first - speed_second);
    var buffer = 5;
+   //--- Length of the modeled road in simulation. We count from 0m to 500m
+   var imaginaryRoadLength = 500;
 
-
-
-   // Calulate for 1st car
+   //--- Calulate for 1st car
    /*
   var delta_t = 2*speed_first/(obstacle_distance - buffer);
   var a = -speed_first/delta_t1;
    */
 
 
-  // Calulate for 2st car
-   var delta_t = (obstacle_distance - buffer)/2*speed_second;
+  //--- Calulate for 2st car
+   var delta_t = (obstacle_distance - buffer)/(2*speed_second);
    var a = -speed_second/delta_t;
+   var x_0 = imaginaryRoadLength - (distance + obstacle_distance); //500m - length of the road
 
 
-
-   //v = - speed_second;
-   //v = -deltav;
-
+   //--- Return calulation results for the second given car
    v = {
-       //"carID": 1,
-       //car1: [-speed_first, delta_t1],
-       //car2: [-speed_second, delta_t2]
-       /*
-       "delta_velocity1": -speed_first,
-       "delta_velocity2": -speed_second,
-       "delta_t1": delta_t1,
-       "delta_t2": delta_t2
-       */
-       //"velocity1": -speed_first/delta_t1,
-       //"velocity2": -speed_second/delta_t2
-
-       "acceleration": a,
-       "delta_t" : delta_t
-
+       "x_0" : x_0,                   //Initial coordinate of 2nd given car
+       "speed_0" : speed_second,      //Initial speed of 2nd given car
+       "acceleration": a,             //Calculated negative acceleration of 2nd given car
+       "delta_t" : delta_t            //Time of velocity change (from the initial speed to a calculated new speed)
     }
 
 
--- socket_io/server.js
+++ socket_io/server.js
@@ -28,96 +28,10 @@ io.on('connection', function(socket) {
                 var car_id = item['car_id']
                 var distance = item['distance']
                 var car_id_speed = tempMemory[car_id][tempMemory[car_id].length - 1]['speed'];
-                var result = Computation.calculate(lastEvent['speed'], car_id_speed, distance, 100);
+                var result = Computation.calculate(lastEvent['speed'], car_id_speed, distance, 300);
                 console.log(result);
                 allClients[`car_${car_id}`].emit('slow_down', result);
             });
         }
     });
 });
-
-
-
-
-
-
-/*
-
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var port = 3000;
-var allClients = {}
-var Computation = require('../MathProcessing/Computation');
-
-server.listen(port, function() {
-    console.log("listening");
-});
-
-app.get('/', function(req, res) {
-    res.send("It's work");
-});
-
-io.on('connection', function(socket) {
-    var clientId = socket.handshake.query.clientId;
-    allClients[clientId] = socket;
-    console.log(`client ${clientId} connected`);
-
-    socket.on('car_event', function(data) {
-
-        //---------------------------------------------------------------
-        //console.log(data);
-        //--- Storing relevant cars' events
-        if (data['car_id'] == '1') {
-            var carOneLastevent = data;
-            ///console.log(carOneLastevent);
-        } else if (data['car_id'] == '2') {
-            var carTwoLastevent = data;
-            ///console.log(carTwoLastevent);
-        } else if (data['car_id'] == '3') {
-            var carThreeLastevent = data;
-            ///console.log(carThreeLastevent);
-        }
-
-        //---------------------------------------------------------------
-        if (data['status'] == 'obstacle') {
-
-
-          //--- Process car event from cars 1 and 2
-          //e.g. params 11m/s , 10m/s , 50m , 30m , 1
-          ///console.log(carOneLastevent);
-          var result = Computation.calculate (11, 10, 50, 30, 1);
-          allClients['car_1'].emit('slow_down', {
-             result
-          });
-
-
-          //--- Process car event from cars 1 and 2
-          ///console.log(carTwoLastevent);
-          allClients['car_2'].emit('slow_down', {
-                velocity: -20
-          });
-
-
-          //--- Process car event from cars 2 and 3
-          allClients['car_3'].emit('slow_down', {
-              velocity: -10
-          });
-
-            var distanceBetwenCars1and2 = carOneLastevent['distances'][0]['distance'];
-            var distanceToObstacle = 50;
-            //console.log(distanceBetwenCars1and2);
-            //console.log(distanceToObstacle);
-
-            //var result = Computation.calculate (carOneLastevent['speed'], carTwoLastevent['speed'], distanceBetwenCars1and2, distanceToObstacle, 1);
-            /*
-
-
-
-
-            //---------------------------------------------------------------
-
-        }
-    });
-});
-*/
